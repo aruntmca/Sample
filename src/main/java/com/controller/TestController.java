9f5d176b3fed9fc941e856836b468b558a3502d8
Arun T <Arun T>
2021-07-19 07:52:41 +0530

    initial version

---
+++ src/main/java/com/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.controller;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/content")
+public class TestController {
+
+	@RequestMapping(value = "/welcome")
+	public String msg() {
+		String str = LocalDateTime.now()
+				.format(DateTimeFormatter.ofPattern("EEEE, dd-MMMM,yyyy HH:mm:ss:SSS a"));
+		return "hello world :: " + str;
+	}
+
+}
